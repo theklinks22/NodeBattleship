3a31693c3309e9333ef9897956f664483377a004
Andrew <klinks22@gmail.com>
2020-04-05 12:09:55 -0400

    port change

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var entities = new Entities();
 var BattleshipGame = require('./app/game.js');
 var GameStatus = require('./app/gameStatus.js');
 
-var port = 3000;
+var port = 3001;
 
 var users = {};
 var gameIdCounter = 1;
